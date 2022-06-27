1b4831c6d7348088f51d4f14b252438b71678da1
ααdhí | αrjun vr <87893573+Aadhi000@users.noreply.github.com>
2022-06-27 22:25:41 +0530

    Add files via upload

---
+++ plugins/extra_feat/neo.py
@@ -0,0 +1,24 @@
+#@Aadhi000 #Neo #neo
+
+import asyncio
+import os
+import math
+import time
+import datetime
+import requests
+from pyrogram import Client, filters
+
+#=====================================================
+BOT_START_TIME = time.time()
+#=====================================================
+
+@Client.on_message(filters.command("ping"))
+async def ping(_, message):
+    start_t = time.time()  
+    avr = await message.reply_text("•••")  
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    uptime = time.strftime("%Hh | %Mm | %Ss", time.gmtime(time.time() - BOT_START_TIME))   
+    await avr.edit(f"‹ ᴄᴜʀʀᴇɴᴛ ʙᴏᴛ sᴛᴀᴛᴜs ›\n\n‹› ᴘᴏɴɢ : {time_taken_s:.3f} ms\n‹› ʙᴏᴛ ᴜᴘᴛɪᴍᴇ : {uptime}")
+    await asyncio.sleep(20)
+    await avr.delete()
